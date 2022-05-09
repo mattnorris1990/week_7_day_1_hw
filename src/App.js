17f537a9a3a00fe1a2ef9abf6778cc93f7ec4dae
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-09 16:10:56 +0100

    hw submission

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [items, setItems] = useState([
+    {name: "Buy Shopping", priority: "high"},
+    { name:"Clean Bathroom", priority: "low"},
+    {name: "Car's MOT", priority: "low"}
+  ])
+
+  const [newDescription, setNewDescription] = useState("Enter new Description here")
+
+  const [newPriority, setNewPriority] = useState("")
+
+  const itemToDisplay = items.map((item, index) => {
+    return (
+      <li key={index} className={item.priority}>
+        {item.name}
+      </li>
+    )
+  })
+
+  const handleItemInput = (event) => {
+    setNewDescription(event.target.value)
+
+  }
+
+  const handleChange = (event) => {
+    setNewPriority(event.target.value)
+  }
+
+  const saveNewItem = (event) => {
+    event.preventDefault()
+    const copyItem = [... items]
+    copyItem.push({name: newDescription, priority: newPriority})
+    setItems(copyItem)
+    setNewDescription("")
+  }
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>To Do's</h1>
+      <hr></hr>
+
+      <ul>
+        {itemToDisplay}
+      </ul>
+
+      <form onSubmit={saveNewItem}>
+        <label htmlFor="new-item">Add a new item:</label>  
+          <input id="new-item" type="text" onChange={handleItemInput} value={newDescription} />     
+        
+        <p>Priority:</p>
+        <label htmlFor="high-priority">High</label>
+        <input
+          type="radio"
+          name="task-priority"
+          value="high"
+          onChange = {handleChange}
+          className="form-check-input"
+        />
+        <label htmlFor="low-priority">Low</label>
+        <input
+          type="radio"
+          name="task-priority"
+          value="low"
+          onChange = {handleChange}
+          className="form-check-input"
+        />
+      
+
+      <input type="submit" value="Save New Item" /> 
+      </form>
+
     </div>
   );
-}
 
+}
 export default App;
